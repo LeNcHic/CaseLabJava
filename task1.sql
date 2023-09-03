86c35b89edc2941424de17f0ae23aa63f5ab780b
Leonid <levin.li@phystech.edu>
2023-09-03 19:33:54 +0300

    reformate_code
    reformate_code

--- task1.sql
+++ task1.sql
@@ -1,22 +1,15 @@
--- Группируем по опыту и нумируем людей внурти каждой группы и сортируем в порядке убывания опыта
-WITH buff_table as (
-  	SELECT
-      	id,
-		surname,
-  		experience,
-    	row_number () over (partition by experience ORDER by id ) as pos_partition
-    FROM student
-  	ORDER by experience DESC
-  )
+-- Группируем по опыту и нумеруем людей внурти каждой группы и сортируем в порядке убывания опыта
+WITH buff_table as (SELECT id,
+                           surname,
+                           experience,
+                           row_number() over (partition by experience ORDER by id ) as pos_partition
+                    FROM student
+                    ORDER by experience DESC)
 
 -- Выбираем топ1 из каждой группы и нумеруем новый список, после чего выводим топ 2
-SELECT
-	surname
-FROM (
-  	SELECT
-  		surname,
-  		row_number () over () as pos
-  	FROM buff_table
-  	WHERE pos_partition = 1
-  )
+SELECT surname
+FROM (SELECT surname,
+             row_number() over () as pos
+      FROM buff_table
+      WHERE pos_partition = 1)
 WHERE pos = 2;
